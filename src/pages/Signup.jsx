5b985a7a1b301863f97d8e21e2b836c0e3a1a41c
Ansh Garg <126083148+anshgarg14877@users.noreply.github.com>
2025-11-28 18:32:28 +0530

    Update signup API endpoint to production URL

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -25,7 +25,7 @@ export default () => {
         const postData = JSON.stringify(formData);
 
         try {
-            const response = await fetch("http://localhost:4000/api/v1/auth/signup", {
+            const response = await fetch("https://discussiondock-backend.onrender.com/api/v1/auth/signup", {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: postData
